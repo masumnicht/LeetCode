8f2bbc071591b1154d641ae988bcd25b8990957a
Matt Sumnicht <masumnicht@gmail.com>
2016-06-02 11:45:13 -0700

    reverse linked list solution

---
+++ reverseLinkedList.js
@@ -0,0 +1,12 @@
+var reverseList = function(head) {
+    var node = head;
+    var prev = null;
+    
+    while (node) {
+        var temp = node.next;
+        node.next = prev;
+        prev = node;
+        node = temp;
+    }
+    return prev;
+};
