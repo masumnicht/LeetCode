8720da2939ca82d3950a1fd87df7aca75c927bcf
Matt Sumnicht <masumnicht@gmail.com>
2016-06-02 11:25:34 -0700

    find duplicate #1 solution

---
+++ findDuplicate1.js
@@ -0,0 +1,10 @@
+var findDuplicate = function(nums) {
+    var obj = {};
+    for (var i = 0; i < nums.length; i++) {
+        if(obj[nums[i]]) {
+            return nums[i];
+        } else {
+            obj[nums[i]] = true;
+        }
+    }
+};
