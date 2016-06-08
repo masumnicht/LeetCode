f998e2ad14bbbe5fdb900a28ae38d03813e92fb5
Matt Sumnicht <masumnicht@gmail.com>
2016-06-08 10:18:28 -0700

    cleanup

--- reverseLinkedList.js
+++ reverseLinkedList.js
@@ -1,12 +1,11 @@
 var reverseList = function(head) {
-    var node = head;
-    var prev = null;
-    
-    while (node) {
-        var temp = node.next;
-        node.next = prev;
-        prev = node;
-        node = temp;
-    }
-    return prev;
+  var node = head;
+  var prev = null;
+  while (node) {
+    var temp = node.next;
+    node.next = prev;
+    prev = node;
+    node = temp;
+  }
+  return prev;
 };
