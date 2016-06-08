e32d4d4f716fa79c3f8037ee8317e9347caa18ec
Matt Sumnicht <masumnicht@gmail.com>
2016-06-08 09:53:52 -0700

    Valid Parentheses

---
+++ validParentheses.js
@@ -0,0 +1,19 @@
+var isValid = function(s) {
+    var stack = [];
+    for (var i=0;i<s.length;i++){
+        if(s[i]==="{"||s[i]==='('||s[i]==='[') {
+            stack.push(s[i]);
+        } else if(s[i]==="}"||s[i]===')'||s[i]===']') {
+            if (stack[stack.length-1]==='('&&s[i]===')'){
+                stack.pop();
+            } else if(stack[stack.length-1]==='['&&s[i]===']'){
+                stack.pop();
+            } else if(stack[stack.length-1]==='{'&&s[i]==='}'){
+                stack.pop();
+            } else {
+                return false;
+            }
+        }
+    }
+    return stack.length === 0;
+};
