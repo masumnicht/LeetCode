fed58cde9877b3da9f3915fc56c302373f24e699
Matt Sumnicht <masumnicht@gmail.com>
2016-06-09 11:55:58 -0700

    Contains Duplicate

---
+++ containsDuplicate.js
@@ -0,0 +1,11 @@
+var containsDuplicate = function (nums) {
+  var obj = {};
+  for (var i=0; i<nums.length; i++) {
+    if (obj[nums[i]]) {
+      return true;
+    } else {
+      obj[nums[i]] = true;
+    }
+  }
+  return false;
+};
